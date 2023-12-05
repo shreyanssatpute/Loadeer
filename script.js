e057f872d5f5f8db5d9bc90b6cea137100f5f6db
shreyanssatpute <136170368+shreyanssatpute@users.noreply.github.com>
2023-12-05 20:21:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const loader = document.getElementById('loader');
-    const name = document.getElementById('name');
+.loader {
+    margin: 0;
+    overflow: hidden;
+    font-family: 'Courier New', Courier, monospace;
+    color: azure;
+}
 
-    // Simulate loading process
-    function simulateLoading() {
-        let width = 0;
-        const interval = setInterval(() => {
-            width++;
-            loader.style.width = width + '%';
+#loader-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #0b0b0b; /* You can set the background color you prefer */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
 
-            if (width === 80) {
-                clearInterval(interval);
-                setTimeout(() => {
-                    name.style.opacity = '1';
-                }, 1);
-            }
-        }, 2);
-    }
+#name {
+    display: block;
+    opacity: 0; /* Initially set opacity to 0 */
+    position: absolute;
+    top: 37%;
+    transform: translateY(-10%);
+    transition: opacity 0.5s ease-in-out; /* Add a smooth opacity transition */
+}
 
-    simulateLoading();
-});
+#loader {
+    width: 0%;
+    height: 7px; /* You can adjust the height of the loader */
+    background: linear-gradient(to right, #c33764, #1d2671);
+    border-radius: 15px;
+}
+
+@media (max-width: 768px) {
+    
+#loader {
+    width: 0%;
+    height: 7px; /* You can adjust the height of the loader */
+    background: linear-gradient(to right, #c33764, #1d2671);
+    border-radius: 15px;
+    transition: 90ms;
+}
+}
